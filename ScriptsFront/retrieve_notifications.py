32f1532b6b01d3aa1c3f354297806b93c2034e5a
Donatien Kamel Junior Ndikum Noupayo <donatien.ndikum-noupayo@etu.u-pec.fr>
2025-01-27 12:54:02 +0100

    mise à jour des scripts

--- ScriptsFront/retrieve_notifications.py
+++ ScriptsFront/retrieve_notifications.py
@@ -1,8 +1,9 @@
 from time import sleep
-
 import requests
+import argparse
 
 BASE_URL = "http://172.31.250.73:8080/notifications"
+
 
 def get_user_notifications(user_id):
     url = f"{BASE_URL}/all/{user_id}"
@@ -12,14 +13,17 @@ def get_user_notifications(user_id):
             notifications = response.json()
             for val in notifications:
                 print(val)
+        else:
+            print(f"Erreur {response.status_code} pour l'utilisateur {user_id}: {response.json()}")
     except requests.exceptions.RequestException as e:
         print(f"Erreur lors de la récupération des notifications pour l'utilisateur {user_id}: {e}")
 
-if __name__ == "__main__":
-    user_id = "1"
-    while True:
-        get_user_notifications(user_id)
-        sleep(5)
 
-    # user_id = "2"
-    # get_user_notifications(user_id)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Script pour récupérer les notifications des utilisateurs.")
+    parser.add_argument("user_id", type=str, help="ID de l'utilisateur pour lequel récupérer les notifications.")
+    args = parser.parse_args()
+
+    while True:
+        get_user_notifications(args.user_id)
+        sleep(5)
